0a6c364a56f32dba51dbfc78bc5b3e19b4706046
Noctua <titgeogeo@gmail.com>
2025-07-02 20:00:27 +0200

    feat: initial Vue 3 + Vite migration setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <h1>Welcome to Your Vue.js App</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,
+      check out the
+    </p>
+  </main>
+</template>
